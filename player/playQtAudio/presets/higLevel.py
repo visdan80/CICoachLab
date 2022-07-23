82b1dcdee1bac8db82d116837dae406b81faf2fd
Daniel Leander <visdan@web.de>
2022-07-23 02:34:06 +0200

    adding another setting

---
+++ player/playQtAudio/presets/higLevel.py
@@ -0,0 +1,6 @@
+def defineSettings(parHandle):
+    """
+    The setting sets increases the level by 20db in relation to the default settings of 65dB
+    """
+
+    parHandle.curPlayer['settings']['level'] = 85
